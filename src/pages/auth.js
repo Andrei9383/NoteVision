3edfa3e217c4a6043c3f8fd921acb139b2c07a22
Andrei9383 <andreistan275@gmail.com>
2023-04-26 00:50:12 +0300

    Fix the link

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,12 +1,12 @@
 import FirebaseAuth from "../../components/auth/FirebaseAuth";
-
+import Link from "next/link";
 const Auth = () => {
   return (
     <div>
       <div>
         <FirebaseAuth />
         <p>
-          <a href="/">Go Home</a>
+          <Link href="/">Go Home</a>
         </p>
       </div>
     </div>
