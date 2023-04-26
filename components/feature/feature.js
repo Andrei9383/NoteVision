d98047c4f12b0d71e86f7229b48b927ddd405525
Andrei9383 <andreistan275@gmail.com>
2023-04-26 22:42:41 +0300

    Fix background

--- components/feature/feature.js
+++ components/feature/feature.js
@@ -27,7 +27,7 @@ const features = [
 
 export default function Features() {
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32 bg-[#fdf6e3]">
+    <div className="overflow-hidden py-24 sm:py-32 bg-[#fdf6e3]">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
