68fe965678c83420a1c44bdfbe2588a11ce1f6c3
Andrei9383 <andreistan275@gmail.com>
2023-04-28 19:08:48 +0300

    Fix auth page

--- components/auth/FirebaseAuth.js
+++ components/auth/FirebaseAuth.js
@@ -108,96 +108,82 @@ const FirebaseAuth = () => {
     <>
       <div>
         {renderAuth ? (
-          // <>
-          //   <div class="flex h-screen  grid-flow-row-dense gap-2 place-items-center">
-          //     <div class="m-auto inline-block text-center">
-          //       <button
-          //         class="px-4 py-2 border w-full place-content-center flex gap-2 m-5 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
-          //         onClick={() => handleLoginGoogle()}
-          //       >
-          //         <img
-          //           class="w-6 h-6"
-          //           src="https://www.svgrepo.com/show/475656/google-color.svg"
-          //           loading="lazy"
-          //           alt="google logo"
-          //         />
-          //         <span>Login with Google</span>
-          //       </button>
-          //       <button
-          //         class="px-4 py-2 border w-full place-content-center flex gap-2 m-5 border-slate-200 rounded-lg text-slate-200 hover:border-slate-400 hover:text-slate-0 hover:shadow transition duration-150 bg-black"
-          //         onClick={() => handleLoginGithub()}
-          //       >
-          //         <div class="w-6 h-6">
-          //           <SvgGithub />
-          //         </div>
-          //         <span>Login with GitHub</span>
-          //       </button>
-          //       <button
-          //         class="px-4 py-2 border w-full place-content-center flex gap-2 m-5 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-0 hover:shadow transition duration-150"
-          //         onClick={() => handleLoginMicrosoft()}
-          //       >
-          //         <div class="w-6 h-6">
-          //           <SvgMicrosoft />
-          //         </div>
-          //         <span>Login with Microsoft</span>
-          //       </button>
-          //     </div>
-          //   </div>
-          // </>
-          <>
-          <div className="bg-[#e9f0ff] ">
-          <div className="relative isolate px-6 pt-14 lg:px-8">
-          <div
-            className="absolute inset-x-0 -top-40  transform-gpu overflow-hidden blur-3xl sm:-top-80"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-cyan-300 to-blue-500 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-            />
-          </div>
-          </div>
-          </div>
-
-          
-          <div class="max-w-sm rounded overflow-hidden shadow-lg flex h-auto m-auto my-10 block text-center bg-[#ffffffff] z-999">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Login using any of the following:</div>
-            <button
-                  class="px-4 py-2 border w-11/12 relative place-content-center flex gap-2 m-5 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150 bg-white"
+          <div className="flex items-center justify-center h-screen">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg flex h-auto m-auto my-10 text-center bg-[#ffffff80] z-999">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">
+                  Login using any of the following:
+                </div>
+                <button
+                  className="px-4 py-2 border w-11/12 relative place-content-center flex gap-2 m-5 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150 bg-white"
                   onClick={() => handleLoginGoogle()}
                 >
-                  <div class="w-6 h-6">
+                  <div className="w-6 h-6">
                     <SvgGoogle />
                   </div>
                   <span>Login with Google</span>
                 </button>
                 <button
-                  class="px-4 py-2 border w-11/12 relative place-content-center flex gap-2 m-5 border-slate-200 rounded-lg text-slate-200 hover:border-slate-400 hover:text-slate-0 hover:shadow transition duration-150 bg-black outline-black"
+                  className="px-4 py-2 border w-11/12 relative place-content-center flex gap-2 m-5 border-slate-200 rounded-lg text-slate-200 hover:border-slate-400 hover:text-slate-0 hover:shadow transition duration-150 bg-black outline-black"
                   onClick={() => handleLoginGithub()}
                 >
-                  <div class="w-6 h-6">
+                  <div className="w-6 h-6">
                     <SvgGithub />
                   </div>
                   <span>Login with GitHub</span>
                 </button>
                 <button
-                  class="px-4 py-2 border w-11/12 relative place-content-center flex gap-2 m-5 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-0 hover:shadow transition duration-150 bg-white"
+                  className="px-4 py-2 border w-11/12 relative place-content-center flex gap-2 m-5 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-0 hover:shadow transition duration-150 bg-white"
                   onClick={() => handleLoginMicrosoft()}
                 >
-                  <div class="w-6 h-6">
+                  <div className="w-6 h-6">
                     <SvgMicrosoft />
-                  </div>  
+                  </div>
                   <span>Login with Microsoft</span>
                 </button>
-                <span>We currently support logging in only using external providers, in order to create a more secure environment.</span>
+                <span>
+                  We currently support logging in only using external providers,
+                  in order to create a more secure environment.
+                </span>
+              </div>
+            </div>
+            <div
+              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+              aria-hidden="true"
+            >
+              <div
+                className="relative left-[calc(50%-20rem)] aspect-[1155/678] w-[27.125rem] -translate-x-1 rotate-[30deg] bg-gradient-to-tr from-cyan-300 to-blue-500 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+              />
+            </div>
+            <div
+              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+              aria-hidden="true"
+            >
+              <div
+                className="relative left-[calc(50%-11rem)] aspect-[650/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-yellow-300 to-blue-500 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+              />
+            </div>
+            <div
+              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+              aria-hidden="true"
+            >
+              <div
+                className="relative right-[calc(0%+11rem)] aspect-[600/600] w-[36.125rem] -translate-x-1/2  rotate-[30deg] bg-gradient-to-tr from-purple-500 to-blue-500 opacity-30 sm:left-[calc(50%+15rem)] sm:w-[72.1875rem]"
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+              />
+            </div>
           </div>
-          </div>
-
-          </>
         ) : null}
       </div>
       {/*{renderAuth ? (
