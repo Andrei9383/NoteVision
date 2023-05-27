fb52cd568dbf09db971b4275c3f1ed96e8fe314b
Andrei9383 <andreistan275@gmail.com>
2023-05-27 03:28:13 +0300

    foxed

--- components/cloudFirestore/CreateNotebook.js
+++ components/cloudFirestore/CreateNotebook.js
@@ -16,7 +16,8 @@ function CreateNotebook(name, user) {
         name,
         created: Timestamp.fromDate(new Date()),
         modified: Timestamp.fromDate(new Date()),
-        content: "",
+        content: '[{"id":"_roEyPQerFpG_9oBZdeZ9","type":"freedraw","x":908,"y":-547,"width":0.0001,"height":0.0001,"angle":0,"strokeColor":"#000000","backgroundColor":"transparent","fillStyle":"hachure","strokeWidth":1,"strokeStyle":"solid","roughness":1,"opacity":100,"groupIds":[],"roundness":null,"seed":238948578,"version":3,"versionNonce":1473094818,"isDeleted":false,"boundElements":null,"updated":1685147206085,"link":null,"locked":false,"points":[[0,0],[0.0001,0.0001]],"pressures":[],"simulatePressure":true,"lastCommittedPoint":[0.0001,0.0001]}]'
+        ,
       });
       return 0;
     } catch (error) {
