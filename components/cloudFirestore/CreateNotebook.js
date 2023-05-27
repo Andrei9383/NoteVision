59d85ff386b5209e8fefc6bd57602020efb3ab3c
Andrei9383 <andreistan275@gmail.com>
2023-05-27 03:09:02 +0300

    d

--- components/cloudFirestore/CreateNotebook.js
+++ components/cloudFirestore/CreateNotebook.js
@@ -1,30 +1,31 @@
-import { db } from '@/lib/firebase/initFirebase'
-import { doc, setDoc, Timestamp } from 'firebase/firestore'
+import { db } from "@/lib/firebase/initFirebase";
+import { doc, setDoc, Timestamp } from "firebase/firestore";
 
-function CreateNotebook (name, user) {
+function CreateNotebook(name, user) {
   // eslint-disable-next-line react/prop-types
-  if (name === '') {
-    console.log('Please enter a name for your notebook')
-    return
+  if (name === "") {
+    console.log("Please enter a name for your notebook");
+    return;
   }
-  if (!user) return
+  if (!user) return;
   const sendData = async () => {
     try {
-      const userDoc = doc(db, user.id, name)
+      const userDoc = doc(db, user.id, name);
       await setDoc(userDoc, {
         // eslint-disable-next-line react/prop-types
         name,
         created: Timestamp.fromDate(new Date()),
-        modified: Timestamp.fromDate(new Date())
-      })
-      return 0
+        modified: Timestamp.fromDate(new Date()),
+        content: "",
+      });
+      return 0;
     } catch (error) {
-      console.log(error)
-      return 1
+      console.log(error);
+      return 1;
     }
-  }
-  sendData()
-  return 0
+  };
+  sendData();
+  return 0;
 }
 
-export default CreateNotebook
+export default CreateNotebook;
